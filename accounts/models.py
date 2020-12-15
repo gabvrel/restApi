0c0ff287c06f47675c335fee4fdab2434ce89c23
gabvrel <gabvrel@gmail.com>
2020-12-15 18:31:00 -0500

    user model modified

--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,6 @@ class User(AbstractUser):
     rate = models.SmallIntegerField(blank=True, default=0)
     reviews = models.SmallIntegerField(blank=True, default=0)
     telephone = models.IntegerField(blank=True, default=0)
-    lat = models.SmallIntegerField(blank=True, default=0)
-    lng = models.SmallIntegerField(blank=True, default=0)
+    lat = models.DecimalField(max_digits=19, decimal_places=16)
+    lng = models.DecimalField(max_digits=19, decimal_places=16)
     radius = models.SmallIntegerField(blank=True, default=0)
